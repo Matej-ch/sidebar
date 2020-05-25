8bb178125a241818742c7c09ba8ccf630215a439
Matej <matejchalachan@gmail.com>
2020-05-25 19:11:38 +0200

    sidebar init commit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <side-navbar/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import SideNavbar from './components/SideNavbar.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    SideNavbar
   }
 }
 </script>
